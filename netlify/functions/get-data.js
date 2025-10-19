e77f33f945e4c3c20319fd689c35ba8da2b734c9
billrodd <107525910+billrodd@users.noreply.github.com>
2025-10-18 22:01:30 -0400

    Create get-data.js

---
+++ netlify/functions/get-data.js
@@ -0,0 +1,30 @@
+// netlify/functions/get-data.js
+import { getStore } from '@netlify/blobs';
+
+export const handler = async (event) => {
+  const cors = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,OPTIONS',
+  };
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 204, headers: cors, body: '' };
+  }
+
+  const store = getStore('race');
+  const data =
+    (await store.get('race-data', { type: 'json' })) || {
+      techs: ['Austin', 'Adam', 'Tim', 'Kaleb'],
+      sales: {
+        Austin: Array(12).fill(0),
+        Adam: Array(12).fill(0),
+        Tim: Array(12).fill(0),
+        Kaleb: Array(12).fill(0),
+      },
+    };
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json', ...cors },
+    body: JSON.stringify(data),
+  };
+};
